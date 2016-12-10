9f8872af7eeaf68d0ebbc3da5417ac66c97ea7de
Thomas Dubiel <td@crazynexus.de>
2016-12-10 21:34:45 +0100

    finish iOS application

--- BMICalculator/ViewController.cs
+++ BMICalculator/ViewController.cs
@@ -1,26 +1,48 @@
 ﻿using System;
 
 using UIKit;
+using Foundation;
 
 namespace BMICalculator
 {
-	public partial class ViewController : UIViewController
-	{
-		protected ViewController(IntPtr handle) : base(handle)
-		{
-			// Note: this .ctor should not contain any initialization logic.
-		}
+   public partial class ViewController : UIViewController
+   {
+      protected ViewController(IntPtr handle) : base(handle)
+      {
+         // Note: this .ctor should not contain any initialization logic.
+      }
 
-		public override void ViewDidLoad()
-		{
-			base.ViewDidLoad();
-			// Perform any additional setup after loading the view, typically from a nib.
-		}
+      public override void ViewDidLoad()
+      {
+         base.ViewDidLoad();
 
-		public override void DidReceiveMemoryWarning()
-		{
-			base.DidReceiveMemoryWarning();
-			// Release any cached data, images, etc that aren't in use.
-		}
-	}
+         calculateButton.TouchUpInside += CalculateButton_TouchUpInside;
+      }
+
+      public override void DidReceiveMemoryWarning()
+      {
+         base.DidReceiveMemoryWarning();
+         // Release any cached data, images, etc that aren't in use.
+      }
+
+      void CalculateButton_TouchUpInside(object sender, EventArgs e)
+      {
+         float height = float.Parse(heightTextField.Text);
+         float weight = float.Parse(weightTextFiel.Text);
+
+         float bmi = (weight / (height * height));
+
+         bmiLabel.Text = bmi.ToString();
+
+         heightTextField.ResignFirstResponder();
+         weightTextFiel.ResignFirstResponder();
+      }
+
+      public override void TouchesBegan(NSSet touches, UIEvent evt)
+      {
+         base.TouchesBegan(touches, evt);
+
+         this.View.EndEditing(true);
+      }
+   }
 }
--- BMICalculator/ViewController.designer.cs
+++ BMICalculator/ViewController.designer.cs
@@ -1,17 +1,50 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace BMICalculator
 {
-	[Register("ViewController")]
+	[Register ("ViewController")]
 	partial class ViewController
 	{
-		void ReleaseDesignerOutlets()
+		[Outlet]
+		UIKit.UILabel bmiLabel { get; set; }
+
+		[Outlet]
+		UIKit.UIButton calculateButton { get; set; }
+
+		[Outlet]
+		UIKit.UITextField heightTextField { get; set; }
+
+		[Outlet]
+		UIKit.UITextField weightTextFiel { get; set; }
+		
+		void ReleaseDesignerOutlets ()
 		{
+			if (bmiLabel != null) {
+				bmiLabel.Dispose ();
+				bmiLabel = null;
+			}
+
+			if (calculateButton != null) {
+				calculateButton.Dispose ();
+				calculateButton = null;
+			}
+
+			if (heightTextField != null) {
+				heightTextField.Dispose ();
+				heightTextField = null;
+			}
+
+			if (weightTextFiel != null) {
+				weightTextFiel.Dispose ();
+				weightTextFiel = null;
+			}
 		}
 	}
 }
