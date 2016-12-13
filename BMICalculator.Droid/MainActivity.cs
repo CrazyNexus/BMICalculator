662900334dcaa469a3d3bccbdbc739a4da926dec
Thomas Dubiel <td@crazynexus.de>
2016-12-13 22:39:24 +0100

    Revert "add new activity"
    This reverts commit 2eac96e9d1e9df109e8bd44890555d78936b5333.

--- BMICalculator.Droid/MainActivity.cs
+++ BMICalculator.Droid/MainActivity.cs
@@ -2,15 +2,14 @@
 using Android.Widget;
 using Android.OS;
 using BMICalculator.Shared;
-using Android.Content;
 
 namespace BMICalculator.Droid
 {
    [Activity(Label = "BMI Calculator", MainLauncher = true, Icon = "@mipmap/icon")]
    public class MainActivity : Activity
    {
-      TextView resultTextView;
       Button calculateButton;
+      TextView resultTextView;
       EditText heightEditText, weightEditText;
 
       protected override void OnCreate(Bundle savedInstanceState)
@@ -22,13 +21,11 @@ namespace BMICalculator.Droid
 
          // find and assign view elements 
          calculateButton = FindViewById<Button>(BMICalculator.Droid.Resource.Id.calculateButton);
-         //         bmiDetailsButton = FindViewById<Button>(BMICalculator.Droid.Resource.Id.showDetailsButton);
          resultTextView = FindViewById<TextView>(BMICalculator.Droid.Resource.Id.resultTextView);
          heightEditText = FindViewById<EditText>(BMICalculator.Droid.Resource.Id.heightEditText);
          weightEditText = FindViewById<EditText>(BMICalculator.Droid.Resource.Id.weigthEditText);
 
          calculateButton.Click += CalculateButton_Click;
-         //        bmiDetailsButton.Click += BmiDetailsButton_Click;
       }
 
       void CalculateButton_Click(object sender, System.EventArgs e)
@@ -40,12 +37,6 @@ namespace BMICalculator.Droid
          float bmi = calc.getBMI();
 
          resultTextView.Text = bmi.ToString();
-      }
-
-      void BmiDetailsButton_Click(object sender, System.EventArgs e)
-      {
-         var intent = new Intent(this, typeof(BMIDetailsActivity));
-         StartActivity(intent);
       }
    }
 }
--- BMICalculator.Droid/Resources/Resource.designer.cs
+++ BMICalculator.Droid/Resources/Resource.designer.cs
@@ -44,29 +44,23 @@ namespace BMICalculator.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int BMIDetailTextView = 2131034112;
-			
-			// aapt resource value: 0x7f050005
-			public const int calculateButton = 2131034117;
-			
-			// aapt resource value: 0x7f050002
-			public const int heightEditText = 2131034114;
-			
-			// aapt resource value: 0x7f050006
-			public const int resultTextView = 2131034118;
-			
-			// aapt resource value: 0x7f050007
-			public const int showDetailsButton = 2131034119;
+			// aapt resource value: 0x7f050004
+			public const int calculateButton = 2131034116;
 			
 			// aapt resource value: 0x7f050001
-			public const int textView1 = 2131034113;
+			public const int heightEditText = 2131034113;
+			
+			// aapt resource value: 0x7f050005
+			public const int resultTextView = 2131034117;
+			
+			// aapt resource value: 0x7f050000
+			public const int textView1 = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView2 = 2131034114;
 			
 			// aapt resource value: 0x7f050003
-			public const int textView2 = 2131034115;
-			
-			// aapt resource value: 0x7f050004
-			public const int weigthEditText = 2131034116;
+			public const int weigthEditText = 2131034115;
 			
 			static Id()
 			{
@@ -82,10 +76,7 @@ namespace BMICalculator.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int BMIDetails = 2130903040;
-			
-			// aapt resource value: 0x7f030001
-			public const int Main = 2130903041;
+			public const int Main = 2130903040;
 			
 			static Layout()
 			{
